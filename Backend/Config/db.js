8d41834c5d5e3a1cac62e0322c35b5a7260e6816
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-07-26 14:30:33 +0530

    Update db.js

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Database connected');
   } catch (error) {
     console.error('Database connection error:', error);
