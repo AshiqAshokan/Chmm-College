f76177b885f967ee9a9032e5f14591d93b39b4cc
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-27 12:09:11 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -5,6 +5,8 @@ const generateToken = (res, userId, userType) => {
     const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
       expiresIn: '30d',
     });
+
+    console.log('Generated token:', token);
   
     res.cookie('jwt', token, {
       httpOnly: true,
