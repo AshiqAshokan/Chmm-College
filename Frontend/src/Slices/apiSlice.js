9468c93da27d40ab91737ba34a6fa6ca3d2364a9
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-27 11:18:17 +0530

    apiSlice.js

--- Frontend/src/Slices/apiSlice.js
+++ Frontend/src/Slices/apiSlice.js
@@ -1,6 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://chmm-college.onrender.com' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://chmm-college.onrender.com',
+  credentials: 'include' 
+});
+ 
 
 export const apiSlice = createApi({
   baseQuery,
