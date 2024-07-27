43dec447faab9db5ed4b7948d4ad15391aefadfa
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-27 10:47:13 +0530

    apiSlice.js

--- Frontend/src/Slices/apiSlice.js
+++ Frontend/src/Slices/apiSlice.js
@@ -1,19 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-// Custom baseQuery to include token in headers
-const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://chmm-college.onrender.com',
-  prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.userInfo?.token; // Assuming your token is stored in auth slice under userInfo
-    if (token) {
-      headers.set('Authorization', `Bearer ${token}`);
-    }
-    return headers;
-  },
-});
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://chmm-college.onrender.com' });
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ['User', 'Student'],
+  tagTypes: ['User','Student'],
   endpoints: (builder) => ({}),
 });
