0eb13b2c40f22467b5c22777595baa1c8e3a3f30
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 12:33:15 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,19 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (req, userId, userType) => {
-    const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
-    });
+  const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 
-    console.log('Generated token:', token);
+  console.log('Generated token:', token);
 
-    if (req.session) {
-        req.session.jwt = token; // Set the token in the session cookie
-        console.log('Session cookie set:', req.session.jwt);
-    } else {
-        console.error('Session is not initialized');
-        throw new Error('Session is not initialized');
-    }
+  if (!req.session) {
+    req.session = {}; // Initialize the session if it's not already initialized
+  }
+
+  req.session.jwt = token; // Set the token in the session cookie
+  console.log('Session cookie set:', req.session.jwt);
 };
 
 module.exports = generateToken;
