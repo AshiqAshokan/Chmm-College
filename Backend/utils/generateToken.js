624eed63963be185f0bc8d70df620d3017ebae70
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 10:14:54 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,5 +1,6 @@
 // generateToken.js
 const jwt = require('jsonwebtoken');
+const { path } = require('../app');
 
 const generateToken = (res, userId, userType) => {
     const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
@@ -13,6 +14,7 @@ const generateToken = (res, userId, userType) => {
       secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
       sameSite: 'none', // Prevent CSRF attacks
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      path: '/',
     });
 };
 
