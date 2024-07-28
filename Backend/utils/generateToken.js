88d6d96c4163030a5f27297307f2e4562b135c3c
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 05:49:23 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,27 +1,20 @@
 // generateToken.js
 const jwt = require('jsonwebtoken');
-const Cookies = require('cookies');
 
-const generateToken = (req, res, userId, userType) => {
-  try {
+
+const generateToken = (res, userId, userType) => {
     const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
       expiresIn: '30d',
     });
 
     console.log('Generated token:', token);
-
-    const cookies = new Cookies(req, res);
-    cookies.set('token', token, {
+    
+    res.cookie('token', token, {
       httpOnly: true,
       secure: true,
       sameSite: 'strict',
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
-    return token;
-  } catch (error) {
-    console.error(error);
-    // Handle the error, e.g., return an error response to the client
-  }
 };
 
 module.exports = generateToken;
