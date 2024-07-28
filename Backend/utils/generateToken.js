f4b9a1e3aebc37fa2ebe6950fc915b8876731765
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 18:02:04 +0530

    authmiddleware.js,generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,19 +1,25 @@
 // generateToken.js
 const jwt = require('jsonwebtoken');
-const { path } = require('../app');
 
 const generateToken = (res, userId, userType) => {
     const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
       expiresIn: '30d',
     });
   
-    res.cookie('jwt', token, {
+    const cookieOptions = {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
       SameSite: 'none', // Prevent CSRF attacks
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       path:'/'
+    };
+  
+    console.log('Cookie:', {
+      jwt: token,
+      options: cookieOptions
     });
+  
+    res.cookie('jwt', token, cookieOptions);
 };
 
 module.exports = generateToken;
