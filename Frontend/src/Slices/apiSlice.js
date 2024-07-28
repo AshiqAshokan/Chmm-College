23dfd9d8f06f75c5f2a9df0d74eeee0fb239c19c
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 14:51:07 +0530

    apislic.js,authSlice.js

--- Frontend/src/Slices/apiSlice.js
+++ Frontend/src/Slices/apiSlice.js
@@ -2,10 +2,16 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({ 
   baseUrl: 'https://chmm-college.onrender.com',
+  prepareHeaders: (headers) => {
+    const token = localStorage.getItem('token'); // Retrieve the token from localStorage or sessionStorage
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
   mode: 'cors',
   credentials: 'include',  // Include credentials in requests
 });
- 
 
 export const apiSlice = createApi({
   baseQuery,
--- Frontend/src/Slices/authSlice.js
+++ Frontend/src/Slices/authSlice.js
@@ -13,10 +13,12 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
+      localStorage.setItem('token', action.payload.token);
     },
     logout: (state, action) => {
       state.userInfo = null;
       localStorage.removeItem('userInfo');
+      localStorage.removeItem('token');
     },
   },
 });
