ee0b26c25bb7fd3f459798d61c0ae76d61b7e0fc
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 09:17:00 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,20 +1,22 @@
-// generateToken.js
-const jwt = require('jsonwebtoken');
-
 const generateToken = (res, userId, userType) => {
-    const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
+  const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 
-    console.log('Generated token:', token);
+  console.log('Generated token:', token);
+
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  const cookieOptions = {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? 'None' : 'Lax',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    path: '/',
+    domain: isProduction ? 'chmm-college.onrender.com' : undefined, // Set the domain attribute
+  };
+
+  res.cookie('jwt', token, cookieOptions);
   
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-      sameSite: 'strict', // Prevent CSRF attacks
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    });
-      console.log('Cookie set:', res.getHeader('Set-Cookie'));
+  console.log('Cookie set:', res.getHeader('Set-Cookie'));
 };
-
-module.exports = generateToken;
