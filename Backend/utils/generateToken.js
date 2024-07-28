21b76c665f6b09fe29ff18fcaea8722b135c85c8
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 10:04:56 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const Cookies = require('cookies');
 
+/**
+ * Generates a JWT token and sets it as a cookie
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {string} userId - User ID to be stored in the token
+ * @param {string} userType - User type to be stored in the token
+ */
 const generateToken = (req, res, userId, userType) => {
   const token = jwt.sign({ userId, userType }, process.env.JWT_SECRET, {
     expiresIn: '30d',
@@ -18,7 +25,8 @@ const generateToken = (req, res, userId, userType) => {
     domain: 'chmm-college.onrender.com', // Set the domain attribute if needed
   });
 
-  console.log('Cookie set:', cookies.get('jwt'));
+  // Use cookies.get() to retrieve the cookie value
+  console.log('Cookie set:', cookies.get('jwt', { signed: true }));
 };
 
 module.exports = generateToken;
