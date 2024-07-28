e7e5112e9413104429a2cde1ee2ac13d256c72af
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 10:21:22 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -12,7 +12,7 @@ const generateToken = (res, userId, userType) => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-      sameSite: 'none', // Prevent CSRF attacks
+      SameSite: 'none', // Prevent CSRF attacks
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       path: '/',
     });
