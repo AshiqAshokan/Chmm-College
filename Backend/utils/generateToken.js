8894dc477e0ca8e76d9c7a341cec53e2497daba9
AshiqAshokan <ashiqappu112@gmail.com>
2024-07-28 10:32:52 +0530

    generateToken.js

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -16,7 +16,7 @@ const generateToken = (res, userId, userType) => {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       path: '/',
     });
-    console.log('Cookie set:', req.cookies.jwt);
+    console.log(res.getHeaders());
 
 };
 
